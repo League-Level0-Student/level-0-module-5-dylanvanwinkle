37f998ed9da41f35391881a7e0d43eff36b0ac6f
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-04-02 13:28:38 -0700

    SkillPracticeIP

--- src/_06_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -8,21 +8,52 @@ public class ForLoopGauntlet {
 //	for (int i = 100; i > -1; i--) {
 		// System.out.println(i);
 		// }
-	/*	for (int i = 2; i < 101; i++) {
-			if (i % 2 == 0) {
-				System.out.println(i);
-	}*/		
-			// for (int i = 1; i < 100; i++) {
-			// if(i % 2 == 0) {
-			// }else {
-			// System.out.println(i);
-			// }
-	/*	for (int i = 1; i < 501; i++) {
-			if (i % 2 == 0) {
-				System.out.println(i + " is even");
-			}else {
-				System.out.println(i + " is odd");
+		/*
+		 * for (int i = 2; i < 101; i++) { if (i % 2 == 0) { System.out.println(i); }
+		 */
+		// for (int i = 1; i < 100; i++) {
+		// if(i % 2 == 0) {
+		// }else {
+		// System.out.println(i);
+		// }
+		/*
+		 * for (int i = 1; i < 501; i++) { if (i % 2 == 0) { System.out.println(i +
+		 * " is even"); }else { System.out.println(i + " is odd"); } }
+		 */
+		/*
+		 * for (int i = 7; i < 778; i++) { if (i % 7 == 0) { System.out.println(i); }
+		 */
+		// for (int i = 0; i < 12; i++) {
+		// System.out.println("In " + (i + 2008) + " I was " + i + " years old");
+		// }
+		/*
+		 * for (int i = 0; i < 3; i++) {
+		 * 
+		 * for (int j = 0; j < 3; j++) { System.out.println(i + "" + j); } }
+		 */
+		/*
+		 * int c = 1; for (int i = 0; i < 3; i++) {
+		 * 
+		 * for (int j = 0; j < 3; j++) { System.out.print(c); c++; }
+		 * System.out.println(); }
+		 */
+	/*	int c = 1;
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j < 10; j++) {
+				System.out.print(c + "\t");
+				c++;
 			}
-	} */
-}
+			System.out.println();
+		} */
+	/*	int c = 1;
+		for (int i = 0; i < 6; i++) {
+
+			for (int j = 0; j < (1 + i); j++) {
+				System.out.print("* ");
+				c++;
+			}
+			System.out.println();
+		} */
+	}
 }
